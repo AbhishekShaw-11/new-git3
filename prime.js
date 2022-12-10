df8c192ba3ac4a18a2f4e20fe5842dd79cb29896
Upuri07 <115460295+Upuri07@users.noreply.github.com>
2022-12-10 18:36:32 +0530

    code corrected
    I have corrected code

--- prime.js
+++ prime.js
@@ -1,8 +1,9 @@
 let num=13;
+Var count=0;
 for(let i=0;i<=num;i++){
-if(num/i==0){
-count+}}
+if(num%i==0){
+count++}}
 if(count==2){
-console.log(num, is prime);
+console.log(num, "is prime");
 else{
-console.log(its a not prime number)}
+console.log("its a not prime number")}
