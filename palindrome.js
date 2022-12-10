501c0820de03f354eb949b6e51932e7a061eed2f
abhishek 1199 <abhishekshaw1199@gmail.com>
2022-12-10 17:43:52 +0530

    check it

---
+++ palindrome.js
@@ -0,0 +1,6 @@
+let str="aman";
+for(let i=str.length;i>=0;i-+){
+rev+=str;}
+if(rev==str){
+console.log(it a palindrome)}
+else{console.log(not a palindrome
---
+++ prime.js
@@ -0,0 +1,8 @@
+let num=13;
+for(let i=0;i<=num;i++){
+if(num/i==0){
+count+}}
+if(count==2){
+console.log(num, is prime);
+else{
+console.log(its a not prime number)}
